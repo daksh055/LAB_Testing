24a55b6336994f8fe0a9a92141055dd6a4fe8454
Daksh <dakshdosi055@gmail.com>
2026-01-14 15:22:11 +0530

    done

---
+++ Jan2026_Test/src/test/java/pac2/lab10.java
@@ -0,0 +1,23 @@
+package pac2;
+
+public class lab10 {
+	public static boolean StringIsPositive(String str) {
+		  String sb = str.toUpperCase();
+		  if(str.length() == 0) return true;
+		   
+		  
+		 for(int i=0;i<str.length()-1;i++) {
+			 if(str.charAt(i) > str.charAt(i+1)) return false;
+		 }
+		 return true;
+	  }
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		  String str = "apple";
+		  
+		  System.out.println(StringIsPositive(str));
+
+	}
+
+}
