2ff045a614f612d13c86f2cad072111fa76cad65
Daksh <dakshdosi055@gmail.com>
2026-01-15 15:32:54 +0530

    lab 12 to 15

---
+++ Jan2026_Test/src/test/java/pac2/lab14.java
@@ -0,0 +1,26 @@
+package pac2;
+import java.time.ZonedDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+
+ 
+public class lab14 {
+ 
+    public static void printDateTimeByZone(String zoneId) {
+        ZonedDateTime zonedDateTime = ZonedDateTime.now(ZoneId.of(zoneId));
+        DateTimeFormatter df = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+ 
+        System.out.println("Current Date & Time in " + zoneId + ": " + zonedDateTime.format(df));
+    }
+ 
+    public static void main(String[] args) {
+        printDateTimeByZone("America/New_York");
+        printDateTimeByZone("Europe/London");
+        printDateTimeByZone("Asia/Tokyo");
+        printDateTimeByZone("Australia/Sydney");
+    }
+}
+
+
+
+
