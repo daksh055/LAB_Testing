89375c15a7cdef342f858f230e1c0cd4d0fe6162
Daksh <dakshdosi055@gmail.com>
2026-01-23 12:32:34 +0530

    done

---
+++ Jan2026_Test/src/test/java/COREJAVA/lab2.java
@@ -0,0 +1,18 @@
+package pac2;
+
+class Account{
+	
+	long accNUm;
+	double balance;
+	accHolder
+	
+}
+public class lab2 {
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Jan2026_Test/src/test/java/COREJAVA/lab4_4.java
@@ -0,0 +1,19 @@
+package pac2;
+import java.io.*;
+
+public class lab4_4 {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+FileInputStream fis=new FileInputStream("C:\\Users\\Daksh.Dosi\\git\\Testing2026\\Jan2026_Test\\src\\test\\java\\pac2\\test.txt");
+		
+		int data;
+		while((data=fis.read())!=-1)
+		{
+			if((int)data%2==0 && (char)data!=',')
+			System.out.print((char)data+" ");
+		}
+		fis.close();
+
+	}
+}
